c3e88ab8276822f7dd489b7ce9f19d7d2987a99f
huaiyizhao <huaiyizhao@tencent.com>
2026-05-29 17:32:01 +0800

    increase rs thres

--- fully_async_gui_agent/run_fully_async_gui_agent_32b_6_2.sh
+++ fully_async_gui_agent/run_fully_async_gui_agent_32b_6_2.sh
@@ -121,7 +121,7 @@ rollout_correction_bypass_mode=${rollout_correction_bypass_mode:-True}
 rollout_correction_loss_type=${rollout_correction_loss_type:-ppo_clip}
 rollout_correction_is=${rollout_correction_is:-null}
 rollout_correction_rs=${rollout_correction_rs:-seq_mean_k1}
-rollout_correction_rs_threshold=${rollout_correction_rs_threshold:-0.999_1.001}
+rollout_correction_rs_threshold=${rollout_correction_rs_threshold:-0.5_2.0}
 case "${rollout_correction_bypass_mode}" in
     True|true|TRUE|1)
         actor_policy_loss_mode=${actor_policy_loss_mode:-bypass_mode}
