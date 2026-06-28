cdc0e53845130ad4ba3de6ef38527dcd8c8e208b
huaiyizhao <huaiyizhao@tencent.com>
2026-06-28 16:49:07 +0800

    no mlflow dep

--- fully_async_gui_agent/run_v1_gui_agent_fully_async.sh
+++ fully_async_gui_agent/run_v1_gui_agent_fully_async.sh
@@ -306,9 +306,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.agent.agent_loop_config_path=${agent_loop_config_path} \
     actor_rollout_ref.rollout.agent.default_agent_loop=gui_agent \
     actor_rollout_ref.rollout.agent.num_workers=16 \
-    actor_rollout_ref.rollout.trace.backend=mlflow \
-    actor_rollout_ref.rollout.trace.token2text=True \
-    trainer.logger='["console", "mlflow"]' \
+    trainer.logger='["console"]' \
     trainer.balance_batch=False \
     trainer.project_name="${project_name}" \
     trainer.experiment_name="${experiment_name}" \
